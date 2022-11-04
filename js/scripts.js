987c24fc88d70f272d79319d55a35a39bd2c8cfe
robbryandev <robbryandev@gmail.com>
2022-11-04 09:53:58 -0700

    Change pizza card background on hover

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,23 @@
 
 // Ui
 
+function cardHoverColor() {
+  document.querySelectorAll(".card").forEach(function(card) {
+    card.addEventListener("mouseenter", function(event) {
+      setTimeout(function() {
+        event.target.classList.remove("bg-my-light");
+        event.target.classList.add("bg-danger");
+      }, 50);
+    });
+    card.addEventListener("mouseleave", function(event) {
+      setTimeout(function() {
+        event.target.classList.remove("bg-danger");
+        event.target.classList.add("bg-my-light");
+      }, 50);
+    })
+  });
+}
+
+addEventListener("load", function() {
+  cardHoverColor();
+});
